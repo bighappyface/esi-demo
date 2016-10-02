5be947e175680425be779bef73ef57f11ee16d12
bighappyface <anomalous20@gmail.com>
2016-10-02 09:06:32 -0500

    refactor(provision-edge): use Varnish 4.1 rpm

--- provision-edge.sh
+++ provision-edge.sh
@@ -4,7 +4,7 @@
 yum install epel-release -y
 
 # Configure Varnish 4.1 RPM
-rpm --nosignature -i https://repo.varnish-cache.org/redhat/varnish-4.0.el6.rpm
+rpm --nosignature -i https://repo.varnish-cache.org/redhat/varnish-4.1.el6.rpm
 
 # Install Varnish
 yum install varnish -y
